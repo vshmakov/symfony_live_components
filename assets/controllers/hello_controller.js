f6e4515b7232e72a36e93c4f86a29851736e0a35
VadimShmakov <shmakovvadim@mail.ru>
2023-09-05 16:40:45 +0500

    Initial commit

---
+++ assets/controllers/hello_controller.js
@@ -0,0 +1,20 @@
+import {Controller} from '@hotwired/stimulus';
+
+/*
+ * This is an example Stimulus controller!
+ *
+ * Any element with a data-controller="hello" attribute will cause
+ * this controller to be executed. The name "hello" comes from the filename:
+ * hello_controller.js -> "hello"
+ *
+ * Delete this file or adapt it for your use!
+ */
+export default class extends Controller {
+  static targets = ["name"]
+
+  greet() {
+    const element = this.nameTarget
+    const name = element.value
+    console.log(`Hello, ${name}!`)
+  }
+}
