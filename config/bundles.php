353ff9103267660732a14f6389ee4fd93f7197b7
VadimShmakov <shmakovvadim@mail.ru>
2023-09-05 19:56:47 +0500

    Installed orm

--- config/bundles.php
+++ config/bundles.php
@@ -8,4 +8,6 @@ return [
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     Symfony\UX\TwigComponent\TwigComponentBundle::class => ['all' => true],
     Symfony\UX\LiveComponent\LiveComponentBundle::class => ['all' => true],
+    Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
+    Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
 ];
